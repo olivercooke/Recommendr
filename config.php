bbf4c6eb1d227363096d36f4260705777907e10e
olivercooke <142438452+olivercooke@users.noreply.github.com>
2023-09-01 19:33:43 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
    define("DB_SERVER", "localhost");
-   define("DB_USERNAME", "id21210434_root");
-   define("DB_PASSWORD", "Password12$");
-   define("DB_DATABASE", "id21210434_recommendr");
+   define("DB_USERNAME", "root");
+   define("DB_PASSWORD", "");
+   define("DB_DATABASE", "recommendr");
    
     $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 if (!$conn) {
